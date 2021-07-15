416c32c87fd0cecbe4097f6e2df9edd9d0ebce98
AG <knightauguest@gmail.com>
2021-07-15 15:49:08 -0400

    finished Gatsby tutorial part 2

---
+++ src/pages/about.jsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import Layout from "../components/layout";
+
+const AboutPage = () => (
+  <Layout pageTitle="About me">
+    <p>
+      Greetings. My name is <strong>Auguest</strong> with an 'e', a former
+      Mechanical Engineer turned into software developer.
+    </p>
+  </Layout>
+);
+
+export default AboutPage;
